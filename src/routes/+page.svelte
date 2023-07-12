67d6a5d487660f22996e438ba4634689c0950f3e
Logan Woolf <loganwoolf@gmail.com>
2023-07-11 23:39:56 -0600

    Create initial layout, input and clear button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="app">
+	<h1 class="title">definite-ly</h1>
+
+	<div class="search flex flex-column align-center">
+		<label class="search--label" for="word">
+			<input
+				class="search--input"
+				id="word"
+				name="word"
+				placeholder="enlighten yourself"
+				type="text"
+			/>
+		</label>
+		<button class="search--clear">clear</button>
+	</div>
+</div>
+
+<style>
+	.app {
+		border: var(--border-base);
+		box-shadow: var(--drop-shadow);
+		padding: 1rem 2rem;
+	}
+
+	.title {
+		margin-bottom: 1rem;
+	}
+
+	.search {
+		align-items: center;
+		display: flex;
+		flex-direction: column;
+		gap: 0.75rem;
+	}
+
+	.search * {
+		display: block;
+		width: 100%;
+	}
+
+	.search--input,
+	.search--clear {
+		text-align: center;
+		padding: 0.33rem 2rem;
+	}
+
+	.search--input:hover:not(:focus) {
+		box-shadow: var(--drop-shadow-semi);
+	}
+
+	.search--clear:hover {
+		box-shadow: var(--drop-shadow);
+		cursor: pointer;
+	}
+
+	.search--clear:active {
+		box-shadow: unset;
+		translate: 4px 4px;
+	}
+</style>
